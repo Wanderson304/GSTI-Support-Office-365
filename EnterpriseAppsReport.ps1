700ab1e5db612ef275290de80015578e915250c5
Wanderson Silva <ca-scudo@hotmail.com>
2025-06-11 11:06:10 -0300

    Update EnterpriseAppsReport.ps1

--- EnterpriseAppsReport.ps1
+++ EnterpriseAppsReport.ps1
@@ -7,28 +7,14 @@ Version:        1.0
 ============================================================================================
 #>
 
-Get-InstalledModule Microsoft.Graph
-Get-InstalledModule
-Set-ExecutionPolicy -ExecutionPolicy RemoteSigned -Scope CurrentUser
+#Get-InstalledModule Microsoft.Graph
+#Get-InstalledModule
+#Set-ExecutionPolicy -ExecutionPolicy RemoteSigned -Scope CurrentUser
 
-Install-Module Microsoft.Graph -Scope CurrentUser -Repository PSGallery -Force
-Install-Module Microsoft.Graph.Beta -Repository PSGallery -Force
+#Install-Module Microsoft.Graph.Beta -Repository PSGallery -Force
+#Import-Module Microsoft.Graph -ErrorAction Stop
 
-<#
-.SYNOPSIS
-    Exporta um relatório CSV com as datas de expiração das chaves (client secrets e certificados) dos aplicativos registrados no Entra ID (Azure AD) via Microsoft Graph.
 
-.DESCRIPTION
-    Este script usa o módulo Microsoft.Graph para listar todos os aplicativos (Azure AD Apps),
-    extrair informações sobre secrets (passwordCredentials) e certificados (keyCredentials),
-    e exportar um relatório contendo nome do app, AppId, tipo de credencial e data de expiração.
-
-.NOTES
-    Pré-requisitos:
-      - Microsoft.Graph instalado (Install-Module Microsoft.Graph -Scope CurrentUser)
-      - Permissões: Application.Read.All
-      - Concessão de consentimento de administrador pode ser necessário.
-#>
 
 param (
     [string]$OutputPath = (Join-Path -Path (Get-Location) -ChildPath ("AppCredentialExpirations_{0}.csv" -f (Get-Date -Format "yyyyMMdd_HHmmss")))
@@ -36,9 +22,9 @@ param (
 
 try {
     if (-not (Get-Module -ListAvailable -Name Microsoft.Graph)) {
-        Install-Module Microsoft.Graph -Scope CurrentUser -Force
+        #Install-Module Microsoft.Graph -Scope CurrentUser -Force
     }
-    Import-Module Microsoft.Graph -ErrorAction Stop
+        #Import-Module Microsoft.Graph -ErrorAction Stop
 
     $Scopes = @("Application.Read.All")
     Write-Host "Autenticando no Microsoft Graph..."
@@ -89,6 +75,5 @@ finally {
     Disconnect-MgGraph | Out-Null
 }
 
-#cd "D:\PowerShell\01 - Labs\02 - Chaves de App"
 
-.\EnterpriseAppsReport.ps1
+start C:\Windows\system32\AppCredentialExpirations_20250611_110501.csv
